8f283cdd4e2eb4e652e690725935ff847ec024b5
Marcelo Brito <marcelobritomed@gmail.com>
2021-08-03 20:24:50 -0300

    Segunda Versao(funcionando)

--- index.js
+++ index.js
@@ -9,50 +9,25 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
 
-
-  // Adds support for GET requests to our webhook
-  app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "687697657567dxfsx"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
-  });
-
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
   let body = req.body;
 
   // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+  if (body&&body.object === 'page') {
 
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      entry.messaging.forEach(function(event){
+        if(event.message){
+          trataMensagem(event);
+        }
+      });
+      //console.log(webhook_event);
     });
 
     // Returns a '200 OK' response to all requests
@@ -64,6 +39,34 @@ app.post('/webhook', (req, res) => {
 
 });
 
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "687697657567dxfsx";
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
 function trataMensagem(event){
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -71,8 +74,3 @@ function trataMensagem(event){
   var message = event.message;
   console.log("Mensgem recebida pelo usurario %d pela página %d", senderID, recipientID);
 }
-
-
-
-
-
